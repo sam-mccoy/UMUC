c99fc6eee97205ea17dd3d17bd8de8e90f2dccb6
Jen Rasmussen <jrasmussen@four51.com>
2016-10-26 19:43:39 -0500

    Featured Items - Bags renaming fixes

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,5 @@
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
     'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-Carousel', 'OrderCloud-ContactModal', 'OrderCloud-FAQModal', 'OrderCloud-FeaturedItemsAccessories',
-    'OrderCloud-FeaturedItemsApparel', 'OrderCloud-FeaturedItemsBag', 'OrderCloud-FeaturedItemsDrinkware', 'OrderCloud-FixedFooter', 'OrderCloud-HamburgerNavigation', 'OrderCloud-ProductZoom',
+    'OrderCloud-FeaturedItemsApparel', 'OrderCloud-FeaturedItemsBags', 'OrderCloud-FeaturedItemsDrinkware', 'OrderCloud-FixedFooter', 'OrderCloud-HamburgerNavigation', 'OrderCloud-ProductZoom',
     'OrderCloud-SecurityModal', 'OrderCloud-ProductSearchInput']);
